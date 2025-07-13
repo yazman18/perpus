9f89d5624f4072c57a7520781500a7e90c6e262e
d0ughnuts4u <aldinorasend@gmail.com>
2025-07-13 19:54:47 +0700

    style(card) : card in books catalog

--- resources/js/Pages/KatalogPage.jsx
+++ resources/js/Pages/KatalogPage.jsx
@@ -88,43 +88,43 @@ const KatalogPage = () => {
                 </div>
 
                 {/* 📚 Catalog List */}
-                <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-8 bg-gray-200 p-6 rounded-md">
+                <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-6 gap-8 bg-gray-200 p-6 rounded-md">
                     {booksData?.map((book) => (
                         <Link
                             href={`/books/${book.id}`}
                             key={book.id}
-                            className="bg-white p-4 flex flex-col items-center  hover:scale-[1.02] transition duration-300 ease-in-out transform rounded-lg shadow-md"
+                            className="flex flex-col px-4 py-4 bg-[#F5F7FA] hover:scale-[1.02] transition duration-300 ease-in-out transform rounded-lg  shadow-2xl"
                         >
                             <Image
                                 src={book.image}
                                 alt={book.title}
                                 w={200}
                                 h={240}
-                                className="max-w-full h-[240px] items-center content-center justify-center object-cover mb-3 rounded-lg "
+                                className="w-full h-[200px] items-center content-center justify-center object-fill mb-3 rounded-lg "
                             />
-                            <h4 className="text-sm font-bold text-center text-gray-800 mb-2 ">
+                            <h4 className="text-sm font-bold text-left text-[#165BAA]">
                                 {book.title}
+                                
                             </h4>
                             <div className=" w-full">
-                                <p className="text-xs text-left text-gray-600 ">
-                                    {book.author} <br /> {book.publisher} (
-                                    {book.year})
-                                </p>
-                                <p className="text-xs text-left items-center text-gray-500 mt-2">
-                                    ISBN: {book.isbn} <br /> {book.pages} pages
+                                <p className="text-xs text-left text-[#1E81B0]">
+                                  by  {book.author} 
                                 </p>
                                 {book.description && (
-                                    <p className="text-xs text-gray-600 mt-2 text-justify">
+                                    <p className="text-xs mt-2 text-[#0B2545] text-justify">
                                         {book.description.length > 100
-                                            ? book.description.substring(0, 85) +
+                                            ? book.description.substring(0, 40) +
                                             "..."
                                             : book.description}
                                     </p>
                                 )}
+                                <p className="text-xs  text-[#6B7280] mt-2">
+                                    ISBN: {book.isbn} <br /> {book.pages} Pages
+                                </p>
                             </div>
                             <Link
                                 href={`/books/${book.id}`}
-                                className="mt-3 inline-block text-xs text-green-600 font-semibold hover:underline"
+                                className="mt-3 inline-block text-center text-xs text-blue-600 font-semibold hover:underline"
                             >
                                 Lihat Detail
                             </Link>
